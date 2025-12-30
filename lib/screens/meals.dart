2acbb3e6492584a99fa8ef8ca64b7fc96c91dc22
greenaltambe <154662822+greenaltambe@users.noreply.github.com>
2025-12-30 11:04:03 +0530

    Add meal list page style

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meals.dart';
+import 'package:meals/widgets/meal_list_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
@@ -12,12 +13,7 @@ class MealsScreen extends StatelessWidget {
     Widget content = ListView.builder(
       itemCount: meals.length,
       itemBuilder: (context, index) {
-        return Text(
-          meals[index].title,
-          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-            color: Theme.of(context).colorScheme.onSurface,
-          ),
-        );
+        return MealListItem(meal: meals[index]);
       },
     );
 
